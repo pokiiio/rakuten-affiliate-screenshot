d587adbb6b5645c5a6871dec55a34a883a3e57de
pokiiio <32353873+pokiiio@users.noreply.github.com>
2019-05-28 17:07:55 +0900

    Update script.

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    page.setViewport({
+        width: 1200,
+        height: 800
+    });
 
     await page.goto('https://affiliate.rakuten.co.jp/');
+
+    let items = await page.$$('.fa-sign-in-alt');
+
+    items[1].click();
+
+    await page.waitForNavigation();
+
+
     await page.screenshot({
         path: 'rakuten-affiliate.png',
         fullPage: true
